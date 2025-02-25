7047e9046a134f4e5e8c56b9753a8b79fbaba3f0
Achmad Rizki Ramadhan <achmadrizkiramadhan0101@gmail.com>
2025-02-25 16:57:36 +0700

    refactor: set policy for bucket

--- internal/config/s3.go
+++ internal/config/s3.go
@@ -10,6 +10,7 @@ import (
 )
 
 func NewBucket(viper *viper.Viper) *minio.Client {
+	// Get the values from the config file
 	endpoint := viper.GetString("S3_ENDPOINT")
 	accessKeyID := viper.GetString("S3_ID")
 	secretAccessKey := viper.GetString("S3_SECRET_KEY")
@@ -21,13 +22,12 @@ func NewBucket(viper *viper.Viper) *minio.Client {
 		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
 		Secure: useSSL,
 	})
-
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	// Make a new bucket
 	ctx := context.Background()
-
 	err = minioClient.MakeBucket(ctx, bucket_name, minio.MakeBucketOptions{})
 	if err != nil {
 		// Check to see if we already own this bucket (which happens if you run this twice)
@@ -38,7 +38,28 @@ func NewBucket(viper *viper.Viper) *minio.Client {
 			log.Fatalln(err)
 		}
 	} else {
-		log.Printf("Successfully created %s\n", bucket_name)
+		log.Printf("Successfully created bucket %s\n", bucket_name)
+	}
+
+	// Set public policy for bucket
+	policy := `{
+			"Version": "2012-10-17",
+			"Statement": [
+				{
+					"Effect": "Allow",
+					"Principal": {"AWS": ["*"]},
+					"Action": ["s3:GetObject"],
+					"Resource": ["arn:aws:s3:::` + bucket_name + `/*"]
+				}
+			]
+		}`
+
+	// Set bucket policy
+	err = minioClient.SetBucketPolicy(ctx, bucket_name, policy)
+	if err != nil {
+		log.Printf("Error setting bucket policy: %v\n", err)
+	} else {
+		log.Printf("Successfully set public policy for bucket %s\n", bucket_name)
 	}
 
 	return minioClient
